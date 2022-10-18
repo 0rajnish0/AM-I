048c6862ed8b04c695608fffac346aaf8a46cb8c
Rajnish kumar <22f2000625@student.onlinedegree.iitm.ac.in>
2022-10-18 17:23:00 +0530

    Login xml

--- app/src/main/java/com/example/am_i/Login.java
+++ app/src/main/java/com/example/am_i/Login.java
@@ -2,13 +2,44 @@ package com.example.am_i;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    EditText  mEmail, mPassword;
+    Button mloginBtn;
+    TextView mRegister;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mloginBtn = findViewById(R.id.signin);
+        mRegister = findViewById(R.id.register);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+
+
+        mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Location.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/am_i/Register.java
+++ app/src/main/java/com/example/am_i/Register.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "TAG";
 
     EditText mFullName, mEmail, mPassword;
     Button mRegisterBtn;
@@ -32,6 +34,28 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullname = mFullName.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("email is required");
+                    return;
+                }
+
+                if (password.length() < 8 ){
+                    mPassword.setError("password must be => 8 character");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
             }
         });
 
