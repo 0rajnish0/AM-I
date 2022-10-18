beb50ddb3c65095490373241acdbe26a0df2386f
Rajnish kumar <22f2000625@student.onlinedegree.iitm.ac.in>
2022-10-18 16:49:13 +0530

    SignUp page xml

--- app/src/main/java/com/example/am_i/Register.java
+++ app/src/main/java/com/example/am_i/Register.java
@@ -2,13 +2,39 @@ package com.example.am_i;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+
+    EditText mFullName, mEmail, mPassword;
+    Button mRegisterBtn;
+    TextView mloginBtn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFullName = findViewById(R.id.fullname);
+        mEmail = findViewById(R.id.email);
+        mRegisterBtn = findViewById(R.id.register);
+        mloginBtn = findViewById(R.id.signin);
+
+
+        mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+
+
     }
 }
